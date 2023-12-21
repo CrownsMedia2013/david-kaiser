e4933f02d993a3669badda6ef29c8b82fc28e502
CrownsMedia2013 <it@crowns-media.com>
2023-12-21 02:24:10 +0100

    dropdown fix 1

--- slices/header/NavItem/variations/dropdown.vue
+++ slices/header/NavItem/variations/dropdown.vue
@@ -1,12 +1,16 @@
 <template>
-  <li v-if="slice.primary.label" :class="{ 'uk-active': isActive, 'uk-parent' : vertical }" >
-    <a>
+  <li v-if="slice.primary.label" :class="{ 'uk-parent': vertical }">
+    <a :class="{ 'uk-active': isActive }">
       {{ slice.primary.label }}
       <Icon v-if="!vertical" icon="la:angle-down" />
     </a>
     <div v-if="!vertical" class="uk-navbar-dropdown">
       <ul class="uk-nav uk-navbar-dropdown-nav">
-        <li v-for="(item, index) in slice.items" :class="{ 'uk-active': isActive }" :key="index">
+        <li
+          v-for="(item, index) in slice.items"
+          :class="{ 'uk-active': isActive }"
+          :key="index"
+        >
           <DocumentLink :field="item.link">
             {{ item.label }}
           </DocumentLink>
@@ -14,7 +18,11 @@
       </ul>
     </div>
     <ul v-else class="uk-nav-sub">
-      <li v-for="(item, index) in slice.items" :class="{ 'uk-active': isActive }" :key="index">
+      <li
+        v-for="(item, index) in slice.items"
+        :class="{ 'uk-active': isActive }"
+        :key="index"
+      >
         <DocumentLink :field="item.link">
           {{ item.label }}
         </DocumentLink>
