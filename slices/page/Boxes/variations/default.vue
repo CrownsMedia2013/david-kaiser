0a74245ca0747261b59fdf7273b842226ffd4f4d
CrownsMedia2013 <it@crowns-media.com>
2024-01-10 13:26:36 +0100

    boxes fix 01

--- slices/page/Boxes/variations/default.vue
+++ slices/page/Boxes/variations/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tm-slice tm-boxes tm-boxes-default">
     <div class="uk-container uk-container-large">
-      <div class="uk-grid uk-grid-collapse uk-grid-match uk-child-width-1-3@m" uk-grid>
+      <div class="uk-grid uk-grid-collapse uk-grid-match uk-child-width-1-2@m" uk-grid>
         <div v-for="(box, index) in slice.items" :key="index">
           <Box :slice="slice" :box="box" />
         </div>
